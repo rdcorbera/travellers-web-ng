db7919b289636a2a45dc11a076af9cfa532f8ecd
rdcorbera <rdcorbera@gmail.com>
2021-09-08 06:29:32 -0500

    Ng universal ssr (#1)
    * Include Angular Universal SSR
    * Include TransferState to avoid flickering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppComponent } from './app.component';
 import { TravellersComponent } from './home/travellers/travellers.component';
@@ -15,8 +16,9 @@ import { AppRoutingModule } from './app-routing.module';
     TravelerDetailComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
+    TransferHttpCacheModule,
     FormsModule,
     AppRoutingModule
   ],
