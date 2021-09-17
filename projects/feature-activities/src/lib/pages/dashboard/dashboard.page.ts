921c3f00ecc25ec338f40cf9bd76a4d901f4621f
Ricardo Corbera <rdcorbera@gmail.com>
2021-09-17 20:30:42 -0300

    Include linter for app and libraries

--- projects/feature-activities/src/lib/pages/dashboard/dashboard.page.ts
+++ projects/feature-activities/src/lib/pages/dashboard/dashboard.page.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.css']
 })
-export class DashboardPage implements OnInit {
+export class DashboardPage {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
